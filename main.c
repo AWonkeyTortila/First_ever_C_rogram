a08e729294e18b959509a91951b862d3a5599fe1
A Wonkey Tortila <74030195+AWonkeyTortila@users.noreply.github.com>
2022-02-28 12:17:52 -0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+// Wonkey, 2/28/22 12:16 PM
+
+#include <stdio.h>
+
+// I think this is how you do this.
+int main() {
+    Printf("Hello World!/n");
+    printf("This is Wonkey's first ever C program that he wrote on his own!");
+}
+
+/* I have not compiled the code yet, but im quite sure that it will run due to
+    the code being not that difficult to test by visuals. */
