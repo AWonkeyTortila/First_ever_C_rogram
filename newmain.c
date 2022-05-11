c5b94d772c44030f3ad45bb6785ad675f8f266d4
Wonkey <74030195+AWonkeyTortila@users.noreply.github.com>
2022-05-10 21:43:39 -0700

    Create newmain.c

---
+++ newmain.c
@@ -0,0 +1,17 @@
+/* This is a modified version that actually
+works on x86_64 computers (Windows). */
+
+// Wonkey, 5/10/22 9:42 PM
+
+#include <stdio.h>
+
+// I think this is how you do this.
+int main() {
+    printf("Hello World!\n");
+    printf("This is Wonkey's first ever C program that he wrote on his own!\n");
+	  printf("(This is a modified version that actually works [x86_64]).");
+	getchar();
+}
+
+// Code was tested on Windows 10 home (Build 21H2).
+// Compiled with Visual Studio 2019 x86_64 cross tools.
